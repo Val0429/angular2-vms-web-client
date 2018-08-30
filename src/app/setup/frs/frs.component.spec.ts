68df6d6dc5cf1ecdaccb859a9369cc534380d7c1
Unknown <mfirdaus2007@gmail.com>
2018-08-30 10:05:30 +0800

    Add progress bar to frs

--- src/app/setup/frs/frs.component.spec.ts
+++ src/app/setup/frs/frs.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FrsComponent } from './frs.component';
 import { AppTestModule } from '../../app.module.test';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('FrsComponent', () => {
   let component: FrsComponent;
@@ -9,6 +10,7 @@ describe('FrsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       imports: [AppTestModule],
       declarations: [ FrsComponent ]
     })
--- src/app/setup/frs/frs.component.ts
+++ src/app/setup/frs/frs.component.ts
@@ -3,6 +3,7 @@ import { SetupService } from '../../service/setup.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import * as Globals from 'app/globals';
 import { CommonService } from '../../service/common.service';
+import { NgProgress } from 'ngx-progressbar';
 
 @Component({
   selector: 'app-frs',
@@ -17,19 +18,24 @@ export class FrsComponent implements OnInit {
   account: FormControl;
 
   myform: FormGroup;
-  loading: boolean;
-  constructor(private setupService: SetupService, private commonService: CommonService) {
+  constructor(private setupService: SetupService, private commonService: CommonService, private progressService : NgProgress) {
     //instantiate empty form
     this.createFormControls({});
     this.createForm();
   }
 
   async ngOnInit() {
-    //wait to get setting from server
-    let setting = await this.setupService.getServerSettings();
-    if (setting && setting.frs) {
-      this.createFormControls(setting.frs);
-      this.createForm();
+    try{
+      this.progressService.start();
+      //wait to get setting from server
+      let setting = await this.setupService.getServerSettings();
+      if (setting && setting.frs) {
+        this.createFormControls(setting.frs);
+        this.createForm();
+      }
+    }//no catch, global error handle handles it
+    finally{      
+      this.progressService.done();
     }
   }
   createForm() {
@@ -41,14 +47,20 @@ export class FrsComponent implements OnInit {
     });
   }
   async save() {
-    this.loading = true;
-    var formData = this.myform.value;
-    console.log("frs save setting", formData);
-    let result = await this.setupService.modifyServerSettings({ data: { frs: formData } });
-    console.log("frs save setting result: ", result);
-    let message = (result) ? this.commonService.getLocaleString("common.hasBeenUpdated") : this.commonService.getLocaleString("common.failedToUpdate");
-    this.commonService.showAlert(this.commonService.getLocaleString("pageLayout.setup.frsSetting") + message);
-    this.loading = false;
+    try{
+      this.progressService.start();
+      var formData = this.myform.value;
+      console.log("frs save setting", formData);
+      let result = await this.setupService.modifyServerSettings({ data: { frs: formData } });
+      console.log("frs save setting result: ", result);
+      this.commonService.showAlert(this.commonService.getLocaleString("pageLayout.setup.frsSetting") + this.commonService.getLocaleString("common.hasBeenUpdated"));
+    }//no catch, global error handle handles it
+    finally{      
+      this.progressService.done();
+    }
+  }
+  isLoading():boolean{
+    return this.progressService.isStarted();
   }
   createFormControls(data:any) {
     this.account = new FormControl(data.account, [
