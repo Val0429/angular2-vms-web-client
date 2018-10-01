4d816acb51a0faa4c5c5cf6ebd2cc2c5fa396ff7
Unknown <mfirdaus2007@gmail.com>
2018-10-01 09:58:50 +0800

    Add live visitor update functionality

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -6,6 +6,7 @@
 //export const cgiRoot: string = "http://fis.isapsolution.com:8088/";
 //export const cgiRoot: string = "http://172.16.10.31:8088/";
 export const cgiRoot: string = "http://localhost:6060/";
+export const wsRoot: string = "ws://localhost:6060/";
 export const webRoot: string = "http://localhost:4200/";
 //export const cgiRoot: string = document.location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '') + '/';
 //export const webRoot: string = document.location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '') + '/';
