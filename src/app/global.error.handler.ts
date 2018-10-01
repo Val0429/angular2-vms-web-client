8f13d2c1fce818d4a8ddf160c9a6269b4cf9f04b
Unknown <mfirdaus2007@gmail.com>
2018-10-01 16:10:14 +0800

    Fix login redirector

--- src/app/global.error.handler.ts
+++ src/app/global.error.handler.ts
@@ -1,7 +1,8 @@
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import {LoginService} from 'app/service/login.service';
-import { Router } from '@angular/router';
 import { CommonService } from './service/common.service';
+import { DialogService } from 'ng2-bootstrap-modal';
+import { AlertComponent } from './dialog/alert/alert.component';
 @Injectable()
   /**
    * global error handlers
@@ -25,17 +26,24 @@ export class GlobalErrorHandler implements ErrorHandler {
 
     //error related to REST API responses
     if (error.rejection && error.rejection.status) {
-      
+      console.log(error.rejection.status );
       //whenever we get 401 status, we force user to login again
       if(error.rejection.status === 401) {
-        console.log("forced to logout due to unauthorized token, message: ", error.rejection._body);
-        const loginService = this.injector.get(LoginService);        
-        loginService.invalidateSession();
+        
+        const dialogService = this.injector.get(DialogService);
 
-        commonService.showAlert(commonService.getLocaleString("pageLogin.pleaseRelogin"), commonService.getLocaleString("pageLogin.invalidSession"));
-
-        const routeService = this.injector.get(Router);
-        routeService.navigate(["/login"]);
+        dialogService.addDialog(AlertComponent, {
+          message:commonService.getLocaleString("pageLogin.pleaseRelogin"), 
+          title:commonService.getLocaleString("pageLogin.invalidSession")
+        })
+        .subscribe(() => {
+          console.log("forced to logout due to unauthorized token, message: ", error.rejection._body);
+          const loginService = this.injector.get(LoginService);        
+          loginService.invalidateSession();
+          //const routeService = this.injector.get(Router);
+          //routeService.navigate(["/login"]);
+          location.href="/";  
+        });    
       }
       //whenever we get 400 pop alert
       else if (error.rejection.status === 400) {
